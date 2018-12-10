a3ec5a1dee1c8bfa4a6de125c757ab75ca98b734
Kevin Chen <kchen01@stuy.edu>
2018-12-09 21:38:14 -0500

    deleted files

--- Sort.java
+++ Sort.java
@@ -7,25 +7,41 @@ public class Sort{
     }
     return true;
   }
-  public static void selectionSort(int nums[]) {
-    for (int i = 0; i < nums.length; i ++) {
+  /**Selection sort of an int array.
+    *Upon completion, the elements of the array will be in increasing order.
+    *@param data  the elements to be sorted.
+    */
+    public static void selectionSort(int[] data){
+    for (int i = 0; i < data.length; i ++) {
       // sets the current lowest to the first number
-      int lowest = nums[i];
+      int lowest = data[i];
       //saves the index of the lowest number
       int index = i;
       //loops through to find a number less than i then save it and it's index
-      for (int j = i; j < nums.length; j++) {
+      for (int j = i; j < data.length; j++) {
         // only take it in if it is the lowest so far
-        if (nums[j] < lowest) {
-          lowest = nums[j];
+        if (data[j] < lowest) {
+          lowest = data[j];
           index = j;
         }
   }
   // need a temporvary variable to allow a swap to happen
-  int temp = nums[i];
+  int temp = gata[i];
   nums[i] = nums[index];
   nums[index] = temp;
 }
+
+}
+
+
+
+  /**Bubble sort of an int array.
+  *Upon completion, the elements of the array will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
+  public static void bubbleSort(int[] data){
+
+
 
 }
 }
